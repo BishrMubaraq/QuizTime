15dc27af53e57ccf1961f15ca4b3e04768c821d6
Bishr Mubarak <bishrmubaraq@gmail.com>
2022-03-27 11:44:08 +0530

    Create Users,Setup Login and Signup

---
+++ helpers/user-helpers.js
@@ -0,0 +1,38 @@
+var db=require('../config/connection')
+var collections=require('../config/collections')
+const bcrypt=require('bcrypt')
+var objectId=require('mongodb').ObjectId
+
+module.exports={
+    doSignup:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            userData.Password=await bcrypt.hash(userData.Password,10)
+            db.get().collection(collections.USER_COLLECTION).insertOne(userData).then((data)=>{
+                userData._id = data.insertedId
+                resolve(userData)
+            })
+        })
+        
+    },
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let user = await db.get().collection(collections.USER_COLLECTION).findOne({Email:userData.Email})
+            if(user){
+                bcrypt.compare(userData.Password,user.Password).then((status)=>{
+                    if(status){
+                        response.user=user
+                        response.status=true
+                        resolve(response)
+                    }else{
+                        resolve({status:false})
+                    }
+                })
+
+            }else{
+                resolve({status:false})
+            }
+        })
+    },
+}
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const categoryHelpers = require('../helpers/category-helpers');
+const userHelpers = require('../helpers/user-helpers');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/home',{user:true});
+  categoryHelpers.getAllCategories().then((categories)=>{
+    res.render('user/home',{user:true,categories});
+  })
 });
 
+// Login page
+
+router.get('/login',(req,res)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+    res.render('user/login',{login:true,"LoginErr":req.session.loginErr})
+    req.session.loginErr=false
+  }
+});
+
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      req.session.loginErr=true
+      res.redirect('/login')
+    }
+  })
+});
+
+
+// Signup page
+router.get('/signup',(req,res)=>{
+  res.render('user/signup',{user:true})
+})
+
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
+  })
+})
 module.exports = router;
